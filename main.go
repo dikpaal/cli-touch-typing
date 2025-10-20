3e2ec627c935a16067d66d80a1491fa9ca977ddb
dikpaal <dikpaal@icloud.com>
2025-10-19 17:59:58 -0700

    learn how to perform http requests and use .env

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// you have to load the .env first
+	_ = godotenv.Load()
+
+	endpoint := fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=Iasi&days=1&aqi=no&alerts=no", os.Getenv("API_KEY"))
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// close the response body when done
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Error: Unable to fetch weather data")
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+
 }
